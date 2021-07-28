d6ce1c6da9695ce2ad3b88f8fb9bb714635992e9
aabelov2703 <alex.belov.2703@gmail.com>
2021-07-28 19:09:00 +0300

    feat: add Reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let res = 0,
+        digits = n < 0 ? -1 * n : n
+    while (digits  > 0) {
+        res = res * 10 + digits % 10;
+        digits = Math.floor(digits / 10);
+    } ;
+    return res;  
 }
